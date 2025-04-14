cc67ef8578c787ca27a74d12a7eca946f182a0a6
Manish Singh <2701msingh@gmail.com>
2025-04-14 20:30:42 +0530

    User registration

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class UserRegistration {
     private static final String FILE_NAME = "users.txt";
     private static boolean isLoggedIn = false;
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
