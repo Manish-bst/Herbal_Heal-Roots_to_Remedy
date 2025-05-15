2ed0239ceb5ad7e2056b707ee8d46ecca5ddb4ce
Manish Singh <2701msingh@gmail.com>
2025-05-15 22:03:55 +0530

    User Review System

--- UserReviewSystem.java
+++ UserReviewSystem.java
@@ -106,6 +106,7 @@ public class UserReviewSystem {
 
         reviewPanel.add(tabbedPane1, BorderLayout.CENTER);
         tabbedPane.addTab("User   Review System", reviewPanel);
+        
     }
     private int extractRating(String line) {
         try {
