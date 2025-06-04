d64c00fa75b4e78b1d58cfb8aa5ba9fd5ce939ff
Manish Singh <2701msingh@gmail.com>
2025-06-04 21:53:44 +0530

    Update HealthTips.java

--- HealthTips.java
+++ HealthTips.java
@@ -216,7 +216,6 @@ public class HealthTips {
                     break;
                 } else if (found) {
                     tips.append(line).append("\n");
-               
                 }
             }
             if (!found) {
