dd22a587b6987821be3dc72710b33228c322e641
Manish Singh <2701msingh@gmail.com>
2025-10-22 22:21:03 +0530

    Update UserRegistration.java

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
-
 public class UserRegistration {
     private static final String FILE_NAME = "users.txt";
     private boolean isLoggedIn = false;
