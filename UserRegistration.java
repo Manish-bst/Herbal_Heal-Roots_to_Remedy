ee56d443abb85d780d90e90d90e17c9b64e5168f
Manish Singh <2701msingh@gmail.com>
2025-10-27 20:59:21 +0530

    Update UserRegistration.java

--- UserRegistration.java
+++ UserRegistration.java
@@ -96,6 +96,7 @@ public class UserRegistration {
         public void actionPerformed(ActionEvent e) {
             String username = usernameField.getText();
             String password = new String(passwordField.getPassword());
+            
             try {
                 Scanner fileScanner = new Scanner(new File(FILE_NAME));
                 while (fileScanner.hasNextLine()) {
